a38b1c98e2313f588f8b7cc300bc3e2f80891052
Artem Firsov <artem.firsov.qwe@gmail.com>
2025-03-18 00:02:15 +0700

    initial commit

--- app/src/router.js
+++ app/src/router.js
@@ -5,7 +5,7 @@ import HomeView from './components/HomeView.vue'
 const routes = [
   { path: '/:building/:room/:problemtypeid', component: HomeView, props: true}
 ]
-
+ 
 const router = createRouter({
   history: createWebHistory(),
   routes,
