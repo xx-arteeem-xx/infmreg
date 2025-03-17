c3c45e1b8e32ee286be1f14e7a367257144eb30d
Artem Firsov <artem.firsov.qwe@gmail.com>
2025-03-18 00:28:37 +0700

    add workers list

--- app/src/components/HomeView.vue
+++ app/src/components/HomeView.vue
@@ -6,30 +6,49 @@
   });
 
   const problemTypes = [
-    "Проблемы с техникой",
-    "Проблемы с мебелью",
-    "Проблемы с электричеством"
+    "Проблемa с техникой",
+    "Проблемa с мебелью",
+    "Проблемa с электричеством"
   ]
+
+  const dataset = {
+    workers: [
+      "Воробьёв Казимир Оскарович",
+      "Дмитриев Севастьян Викторович",
+      "Константинов Эрик Петрович",
+      "Цветков Ярослав Ростиславович",
+      "Белозёров Ибрагил Робертович",
+      "Кулагина Виргиния Львовна",
+      "Гурьева Лили Арсеньевна",
+      "Дмитриева Юфеза Протасьевна",
+      "Дорофеева Силика Николаевна",
+      "Тимофеева Валентина Артёмовна"
+    ]
+  }
 </script>
 
 <template>
   <div class="container my-5">
     <img src="../assets/logo.png" class="mb-3">
-    <h1>Регистрация обращений</h1>
+    <h1>Регистрация обращения</h1>
     <p class="lead mb-3">Текущая аудитория: {{ building }}-{{ room }}
       <br>Вид проблемы: {{ problemTypes[props.problemtypeid] }}</p>
-    <form action="/">
+    <form :action="$route.fullPath">
       <div class="input-group mb-3">
-        <span class="input-group-text" id="basic-addon1">
+        <label class="input-group-text" for="inputGroupSelect01">
           <i class="fa-solid fa-user"></i>
-        </span>
-        <input type="text" class="form-control" placeholder="ФИО">
+        </label>
+        <select class="form-select" id="inputGroupSelect01">
+          <option selected>ФИО</option>
+          <option v-for="(worker, id) in dataset.workers" :value="id+1">{{ worker }}</option>
+        </select>
       </div>
+
       <div class="input-group mb-3">
         <span class="input-group-text" id="basic-addon1">
           <i class="fa-solid fa-phone"></i>
         </span>
-        <input type="text" class="form-control" placeholder="Номер телефона">
+        <input type="text" class="form-control" placeholder="Номер телефона" maxlength="10" minlength="10" value="+7">
       </div>
       <div class="input-group">
         <span class="input-group-text">
